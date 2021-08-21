f000afb72b2ffe805eb22fc0857e4b6e83955072
Javier <chavi.sennin@gmail.com>
2021-08-20 21:42:15 -0500

    Se agregaron mediaquerys al web component

--- src/components/proyects-desk.js
+++ src/components/proyects-desk.js
@@ -4,7 +4,7 @@ class proyectDesk extends HTMLElement {
         this.attachShadow({ mode: 'open' })
     }
     static get observedAttributes() {
-        return ['titleproyect', 'description', 'img', 'imgalt', 'sizeblock',]
+        return ['titleproyect', 'description', 'img', 'imgalt', 'sizeblock', 'tools']
     }
     attributeChangedCallback(attr, oldVal, newVal) {
         if( attr === 'titleproyect') {
@@ -19,14 +19,14 @@ class proyectDesk extends HTMLElement {
         if( attr === 'imgalt') {
             this.imgalt = newVal
         }
-        if( attr === 'sizeblock') {
-            this.sizeblock = newVal
+        if( attr === 'tools') {
+            this.tools = newVal
         }
     }
     getTemplate() {
         const template = document.createElement('template')
         template.innerHTML = `
-            <div class="proyect__container ${this.sizeblock}">
+            <div class="proyect__container">
                 <div class="proyect__img-container">
                     <img src=${this.img} alt=${this.imgalt}></img>
                 </div>
@@ -36,6 +36,9 @@ class proyectDesk extends HTMLElement {
                     </h1>
                     <p>
                         ${this.description}
+                    </p>
+                    <p>
+                        <b>Desarrollado con:</b> ${this.tools}
                     </p>
                 </div>
             </div>
@@ -50,43 +53,77 @@ class proyectDesk extends HTMLElement {
                     --height-container: 330px;
                     margin: 0;
                     padding: 0;
-                    font-family: var(----prompt);
+                    font-family: var(--prompt);
                     height: var(--height-container);
                 }
-                :host(.full-block){
+                :host(.full--block){
                     width: var(--p100);
                 }
-                :host(.two-blocks){
+                :host(.two--blocks){
                     width: 66.66%;
                 }
-                :host(.a-block){
+                :host(.a--block){
                     width: 33.33%;
                 }
-                
-                .proyect__container {
+                .proyect__container{
                     box-sizing: border-box;
                 }
-                .proyect__img-container {
+                .proyect__img-container{
                     overflow: hidden;
                     width: var(--p100);
                     height: var(--height-container);
                 }
-                .proyect__img-container img {
+                .proyect__img-container img{
                     position: relative;
                     width: var(--p100);
                     bottom: 50%;
                     z-index: -1;
                 }
-                .proyect__txt-wrapper {
+                .proyect__txt-wrapper{
                     width: var(--p100);
                     height: var(--height-container);
                     position: relative;
                     bottom: var(--height-container);;
                     z-index: 2;
                     background-color: rgba(0, 0, 0, .4);
-                    padding: 30px 60px;
+                    padding: 10px 40px;
                     box-sizing: border-box;
                     color: var(--white);
+                    overflow: scroll;
+                }
+                .proyect__txt-wrapper h1{
+                    font-size: var(--font-45);
+                }
+                .proyect__txt-wrapper p{
+                    font-size: var(--font-20);
+                }
+
+                @media screen and (max-width: 1125px) {
+                    :host{
+                        --height-container: 260px;
+                    }
+                    :host(.full--block), :host(.two--blocks), :host(.a--block){
+                        width: var(--p100);
+                    }
+                }
+                @media screen and (max-width: 768px) {
+                    :host{
+                        --height-container: 180px;
+                    }
+                    .proyect__txt-wrapper p{
+                        font-size: var(--font-16);
+                    }
+                    .proyect__txt-wrapper h1{
+                        font-size: var(--font-35);
+                    }
+                }
+                @media screen and (max-width: 518px) {
+                    .proyect__txt-wrapper p{
+                        font-size: var(--font-12);
+                    }
+                    .proyect__txt-wrapper h1{
+                        font-size: var(--font-30);
+                    }
                 }
             </style>
         `
